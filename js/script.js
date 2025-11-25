53aeb11622964a99c8967351a0f61a3ef82884c0
lost-in-noise <nancykikvadze@gmail.com>
2025-11-25 15:17:01 +0400

    add javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "use-strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inputGroups = document.querySelectorAll(".input-group");
+  inputGroups.forEach((group) => {
+    const input = group.querySelector("input");
+
+    if (input.value) {
+      group.classList.add("has-content");
+    }
+    input.addEventListener("focus", function () {
+      group.classList.add("focused");
+    });
+    input.addEventListener("blur", function () {
+      group.classList.remove("focused");
+      if (input === "") {
+        group.classList.remove("has-content");
+      }
+    });
+    input.addEventListener("input", function () {
+      if (input.value === "") {
+        group.classList.remove("has-content");
+      } else {
+        group.classList.add("has-content");
+      }
+    });
+  });
+});
